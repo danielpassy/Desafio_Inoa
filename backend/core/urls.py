761fe6f798e9dc97df3189bc4f4fe08140ab994a
Daniel Passy <danielpassy@hotmail.com>
2023-07-25 15:54:31 -0300

    views for asset and assetRecords

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,1 +1,6 @@
-urlpatterns = []
+from .views import core_api
+from django.urls import path
+
+urlpatterns = [
+    path("", core_api.urls),
+]
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+import datetime
+from django.forms import model_to_dict
+from ninja import NinjaAPI, Schema
+from django.contrib.auth.models import User
 
-# Create your views here.
+from core.models import Asset, UserAlert
+
+core_api = NinjaAPI()
+
+
+class LoginForm(Schema):
+    email: str
+    password: str
+
+
+class Assets(Schema):
+    assets: Asset
+
+
+class Error(Schema):
+    error: str
+
+
+class AssetFilter(Schema):
+    ids: list[str]
+    initial_date: datetime.date | datetime.now() - datetime.timedelta(days=30)
+    end_date: datetime.date | None
+
+
+@core_api.get("/assets", response={200: Assets})
+def assets(request, filters: AssetFilter):
+    assets = Asset.objects.all()
+    if filters.ids:
+        assets = assets.filter(id__in=filters.ids)
+    if filters.initial_date:
+        assets = assets.filter(date__gte=filters.initial_date)
+    if filters.end_date:
+        assets = assets.filter(date__lte=filters.end_date)
+
+    return 200, {"assets": model_to_dict(assets)}
+
+
+@core_api.get("/alerts", response={200: UserAlert})
+def alerts(request):
+    user = request.user
+    alerts = UserAlert.objects.filter(user=user)
+
+    return 200, {"alerts": model_to_dict(alerts)}
+
+
+@core_api.post("/alerts", response={201: UserAlert, 400: Error})
+def create_alerts(request, alert_form: UserAlert):
+    user = request.user
+    alert = UserAlert.objects.filter(asset_id=alert_form.asset_id, user=user).first()
+    if alert:
+        alert(**alert_form.dict()).save()
+        return 200, {"alert": model_to_dict(alert)}
+
+    new_alert = UserAlert(user=user, **alert_form.dict())
+    new_alert.save()
+
+    return 201, {"alert": model_to_dict(new_alert)}
