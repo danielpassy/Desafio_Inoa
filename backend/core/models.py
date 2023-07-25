c29518401934e017c4626844e156c6166e052057
Daniel Passy <danielpassy@hotmail.com>
2023-07-25 15:53:40 -0300

    models for assets

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Asset(models.Model):
+    name = models.TextField()
+    short_name = models.TextField()  # "PETROBRAS   PN      N2"
+    long_name = models.TextField()  # "Petróleo Brasileiro S.A. - Petrobras"
+    symbol = models.TextField()  # "PETR4"
+    start_date = models.DateField(auto_now=True, null=True)
+    interval = models.DurationField(null=True)
+
+
+class UserAlert(models.Model):
+    asset = models.ForeignKey(Asset, on_delete=models.CASCADE)
+    inferior_tunel = models.IntegerField()
+    superior_tunel = models.IntegerField()
+    user = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+
+
+class Currency(models.TextChoices):
+    REAL = "BRL"
+    AMERICAN_DOLAR = "US"
+
+
+class AssetRecord(models.Model):
+    asset = models.ForeignKey(Asset, on_delete=models.DO_NOTHING)
+    price = models.IntegerField()
+    currency = models.TextField(choices=Currency.choices)
+    measured_at = models.DateTimeField()
+
+    class meta:
+        orderering = ["-measure_at"]
