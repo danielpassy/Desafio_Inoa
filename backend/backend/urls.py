9e94c0bd60f7dd0ba0c0a0a11cf6fe10e885b8d4
Daniel Passy <danielpassy@hotmail.com>
2023-07-25 20:38:01 -0300

    change url namespace

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/auth/', include('auth.urls')),
-    path('api/', include('core.urls')),
+    path("admin/", admin.site.urls),
+    path("api/auth/", include("auth_user.urls")),
+    path("api/", include("core.urls")),
 ]
