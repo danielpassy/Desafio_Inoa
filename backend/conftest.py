d517f99b7a4b7f22bc46458440a7ac1ae52b594c
Daniel Passy <danielpassy@hotmail.com>
2023-07-26 05:16:21 -0300

    add user to conftest

--- backend/conftest.py
+++ backend/conftest.py
@@ -1,6 +1,21 @@
 import pytest
 
+from auth_user.models import User
+
 
 @pytest.fixture(autouse=True)
 def use_db(db):
     ...
+
+
+@pytest.fixture()
+def user():
+    return User.objects.create_user(
+        email="teste@teste.com.br", username="teste", password="teste"
+    )
+
+
+@pytest.fixture()
+def client_with_user(user, client):
+    client.force_login(user)
+    return client
