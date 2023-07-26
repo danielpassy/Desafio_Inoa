11857e08eae662f20b2324a56e16ccfd16a194f8
Daniel Passy <danielpassy@hotmail.com>
2023-07-25 23:25:27 -0300

    for some reason this doesnt work

--- backend/core/views.py
+++ backend/core/views.py
@@ -2,10 +2,11 @@ import datetime
 from django.forms import model_to_dict
 from ninja import NinjaAPI, Schema
 from django.contrib.auth.models import User
+from pydantic import validator
 
 from core.models import Asset, UserAlert
 
-core_api = NinjaAPI()
+core_api = NinjaAPI(urls_namespace="core")
 
 
 class LoginForm(Schema):
@@ -14,7 +15,7 @@ class LoginForm(Schema):
 
 
 class Assets(Schema):
-    assets: Asset
+    assets: str
 
 
 class Error(Schema):
@@ -23,8 +24,14 @@ class Error(Schema):
 
 class AssetFilter(Schema):
     ids: list[str]
-    initial_date: datetime.date | datetime.now() - datetime.timedelta(days=30)
+    initial_date: datetime.date
     end_date: datetime.date | None
+
+    @validator("initial_date", pre=True)
+    @classmethod
+    def validate_x(cls, v):
+        if not v:
+            return datetime.datetime.now() - datetime.timedelta(days=30)
 
 
 @core_api.get("/assets", response={200: Assets})
@@ -40,7 +47,7 @@ def assets(request, filters: AssetFilter):
     return 200, {"assets": model_to_dict(assets)}
 
 
-@core_api.get("/alerts", response={200: UserAlert})
+@core_api.get("/alerts", response={200: dict})
 def alerts(request):
     user = request.user
     alerts = UserAlert.objects.filter(user=user)
@@ -48,8 +55,8 @@ def alerts(request):
     return 200, {"alerts": model_to_dict(alerts)}
 
 
-@core_api.post("/alerts", response={201: UserAlert, 400: Error})
-def create_alerts(request, alert_form: UserAlert):
+@core_api.post("/alerts", response={201: dict, 400: dict})
+def create_alerts(request, alert_form: dict):
     user = request.user
     alert = UserAlert.objects.filter(asset_id=alert_form.asset_id, user=user).first()
     if alert:
