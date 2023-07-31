ad63cd0ba24684410ac324fcbac86ab6fa91956d
Daniel Passy <danielpassy@hotmail.com>
2023-07-31 16:23:40 -0300

    fix bug periodic task

--- backend/backend/celery_settings.py
+++ backend/backend/celery_settings.py
@@ -1,6 +1,8 @@
 import os
 
 from celery import Celery
+from django.conf import settings
+from celery.schedules import crontab
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
@@ -15,3 +17,15 @@ celery_instance.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Load task modules from all registered Django apps.
 celery_instance.autodiscover_tasks()
+
+
+@celery_instance.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    from core.tasks import update_available_stocks, update_stock_details
+
+    sender.add_periodic_task(
+        crontab(minute="*/5", hour="9-18"), update_stock_details.s()
+    )
+    sender.add_periodic_task(
+        crontab(minute="0", hour="10"), update_available_stocks.s()
+    )
