cf23f86fb47e4391ce6e567059a8d423dbecdfdf
Daniel Passy <danielpassy@hotmail.com>
2023-08-02 14:42:33 -0300

    added mailgun correct settings

--- backend/adapters/email/mailgun.py
+++ backend/adapters/email/mailgun.py
@@ -4,10 +4,10 @@ import requests
 
 def send_email(msg, email, subject):
     return requests.post(
-        f"https://api.mailgun.net/v3/{settings.DOMAIN}/messages",
+        f"https://api.mailgun.net/v3/{settings.EMAIL_HOST}/messages",
         auth=("api", settings.ADAPTERS_KEY["EMAIL"]["MAILGUN_API_KEY"]),
         data={
-            "from": f"Inoa <postmaster@{settings.DOMAIN}>",
+            "from": f"Inoa <postmaster@{settings.EMAIL_HOST}>",
             "to": email,
             "subject": subject,
             "text": msg,
