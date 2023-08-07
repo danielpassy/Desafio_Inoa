2bb911bf08d443e14ffaac58c255bf80c3c86bb3
Daniel Passy <danielpassy@hotmail.com>
2023-08-07 14:32:12 -0300

    added django smtp

--- backend/adapters/email/__init__.py
+++ backend/adapters/email/__init__.py
@@ -1,4 +1,5 @@
 from importlib import import_module
+import textwrap
 from django.conf import settings
 from .typing import email_template_T
 
@@ -8,7 +9,7 @@ adapter = import_module(settings.ADAPTERS["EMAIL"])
 def send_email(
     template: email_template_T,
     email: str,
-    price: float,
+    price: str,
     stock_symbol: str,
 ):
     msg = available_templates[template].format(
@@ -21,23 +22,28 @@ def send_email(
 
 
 available_templates = {
-    "sell_stock_template": """Dear {person_email},
+    "sell_stock": textwrap.dedent(
+        """
+        Dear {person_email}, 
+        
+        This is a notification to inform you that the stock price for {stock_symbol} has reached {price}, above to the superior tunel.
 
-This is a notification to inform you that the stock price for {stock_symbol} has reached {price}, above to the superior tunel.
+        Thank you for using our service
 
-Thank you for using our service.
-
-Best regards,
-Inoa Company
-
-Manage your alerts at https://inoa.com/alerts""",
-    "buy_stock_template": """Dear {person_email},
-
-This is a notification to inform you that the stock price for {stock_symbol} has reached {price}, below to the inferior tunel.
-
-Thank you for using our service.
-
-Best regards,
-Inoa Company
-Manage your alerts at https://inoa.com/alerts""",
+        Best regards,
+        Inoa Company
+        Manage your alerts at https://inoa.com/alerts
+        """,
+    ).replace("\n", "<br>"),
+    "buy_stock": textwrap.dedent(
+        """
+        Dear {person_email},
+        This is a notification to inform you that the stock price for {stock_symbol} has reached {price}, below to the inferior tunel.
+        
+        Thank you for using our service.
+        
+        Best regards,
+        Inoa Company
+        Manage your alerts at https://inoa.com/alerts"""
+    ).replace("\n", "<br>"),
 }
--- backend/adapters/email/django_smtp.py
+++ backend/adapters/email/django_smtp.py
@@ -3,9 +3,10 @@ from django.conf import settings
 
 
 def send_email(msg, email, subject):
-    send_mail(
-        subject=msg,
-        message=subject,
+    return send_mail(
+        subject=subject,
+        message="",
+        html_message=msg,
         from_email=settings.EMAIL_HOST_USER,
         recipient_list=[email],
     )
